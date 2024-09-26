387bc503c1dab29e3e15f19c5827e68950830db8
Al Zee <z@alz.ee>
2024-09-26 18:10:30 +0800

    section-2 html done

--- public/js/home.js
+++ public/js/home.js
@@ -1,7 +1,19 @@
-const swiper = new Swiper('.swiper-1', {
+const swiper1 = new Swiper('.swiper-1', {
   loop: true,
   pagination: {
-    el: '.swiper-pagination',
+    el: '.swiper-1 .swiper-pagination',
     clickable: true,
+  },
+});
+
+const swiper2 = new Swiper('.swiper-2', {
+  loop: true,
+  pagination: {
+    el: '.swiper-2 .swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-2 .swiper-button-next",
+    prevEl: ".swiper-2 .swiper-button-prev",
   },
 });
