ed3ac5d7923af6e7d931f4e43deac7a7efb478d3
Al Zee <z@alz.ee>
2024-09-27 14:25:41 +0800

    nth

--- src/Controller/NodeController.php
+++ src/Controller/NodeController.php
@@ -20,8 +20,8 @@ class NodeController extends AbstractController
         $this->translator = $translator;
     }
     
-    #[Route('news/{nid}', requirements: ['nid' => '\d+'], name: 'app_node_show')]
-    #[Route('product/{nid}', requirements: ['nid' => '\d+'], name: 'app_product_show')]
+    // #[Route('news/{nid}', requirements: ['nid' => '\d+'], name: 'app_node_show')]
+    // #[Route('product/{nid}', requirements: ['nid' => '\d+'], name: 'app_product_show')]
     public function show(int $nid, Request $request): Response
     {
         // $route = $request->attributes->get('_route');
@@ -41,10 +41,41 @@ class NodeController extends AbstractController
 
         return $this->render('node/detail.html.twig', array_merge($data, $data1));
     }
-    
+
     #[Route('/news', name: 'app_news_list')]
+    public function listNodes(Request $request): Response
+    {
+        $locale = $request->getLocale();
+        $page = $request->query->get('p');
+        $limit = 5;
+        if (is_null($page) || empty($page)) {
+          $page = 1;
+        }
+        $offset = $limit * ($page - 1);
+
+        $data = $this->data->getMisc($request->getLocale());
+        $data1 = [
+          //'nodes' => $nodes,
+          //'path' => $region->getName(),
+          //'page_title' => $this->translator->trans('News'),
+          //'page' => $page,
+          //'page_count' => ceil(count($nodes_all) / $limit),
+          //'regionLabel' => $regionLabel,
+        ];
+
+        return $this->render('news/index.html.twig', array_merge($data, $data1));
+    }
+
+    #[Route('news/{nid}', requirements: ['nid' => '\d+'], name: 'app_node_show')]
+    public function showNews(int $nid, Request $request): Response
+    {
+        $data = $this->data->getMisc($request->getLocale());
+
+        return $this->render('news/show.html.twig', $data);
+    }
+    
     #[Route('/news/{regionLabel?}', name: 'app_news_region_label')]
-    public function index(string $regionLabel, Request $request): Response
+    public function listRegionNodes(string $regionLabel, Request $request): Response
     {
         $locale = $request->getLocale();
         $page = $request->query->get('p');
@@ -82,5 +113,35 @@ class NodeController extends AbstractController
         ];
 
         return $this->render('node/index.html.twig', array_merge($data, $data1));
+    }
+
+    #[Route('/about', name: 'app_about')]
+    public function aboutUs(Request $request): Response
+    {
+        return $this->render('node/about.html.twig');
+    }
+
+    #[Route('/topic', name: 'app_product_topic')]
+    public function topic(Request $request): Response
+    {
+        return $this->render('node/topic.html.twig');
+    }
+
+    #[Route('/products', name: 'app_product_center')]
+    public function listProducts(Request $request): Response
+    {
+        return $this->render('node/products.html.twig');
+    }
+
+    #[Route('/services', name: 'app_services')]
+    public function services(Request $request): Response
+    {
+        return $this->render('node/services.html.twig');
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(Request $request): Response
+    {
+        return $this->render('node/contact.html.twig');
     }
 }
